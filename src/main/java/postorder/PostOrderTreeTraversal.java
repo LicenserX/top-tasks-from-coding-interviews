8e49326dba31c457a730c5ca50f491f081d1225a
LicenserX <Licensent@gmail.com>
2021-09-28 17:47:19 +0300

    PostOrderTreeTraversal.java was completed.

---
+++ src/main/java/postorder/PostOrderTreeTraversal.java
@@ -0,0 +1,79 @@
+package postorder;
+
+import java.util.*;
+
+import static util.ReflectionUtil.*;
+
+public class PostOrderTreeTraversal {
+    public PostOrderTreeTraversal() {
+    }
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        Set<Integer> integerSet = new TreeSet<>();
+        integerSet.add(2);
+        integerSet.add(14);
+        integerSet.add(13);
+        integerSet.add(5);
+        integerSet.add(6);
+        integerSet.add(18);
+        integerSet.add(21);
+        integerSet.add(15);
+
+
+        Object objectM = getNonPublicFieldAsObject(integerSet, "m");
+        TreeMap<Integer, Object> treeMap = (TreeMap<Integer, Object>) objectM;
+
+        Object treeRoot = getNonPublicFieldAsObject(treeMap,"root");
+        Object treeRoot2 = null;
+
+        lrnRecursiveTraversal(treeRoot);
+        System.out.println("");
+        lnrTraversal(treeRoot);
+    }
+
+    public static void lrnRecursiveTraversal(Object treeRoot) throws NoSuchFieldException, IllegalAccessException {
+        if (treeRoot == null) {
+            return;
+        }
+
+        Object leftNode = getNonPublicFieldAsObject(treeRoot, "left");
+        Object rightNode = getNonPublicFieldAsObject(treeRoot, "right");
+
+        lrnRecursiveTraversal(leftNode);
+        lrnRecursiveTraversal(rightNode);
+        System.out.println(treeRoot);
+    }
+
+    public static void lnrTraversal(Object treeRoot) throws NoSuchFieldException, IllegalAccessException {
+        Deque<Object> queue = new ArrayDeque<>();
+
+        if (treeRoot == null) {
+            return;
+        }
+        queue.push(treeRoot);
+
+        while (!queue.isEmpty() ) {
+            Object currentNode = queue.peek();
+
+            if (isLeaf(currentNode)) {
+                System.out.println(queue.pop());
+            } else {
+                Object rightNode = getNonPublicFieldAsObject(currentNode, "right");
+                Object leftNode = getNonPublicFieldAsObject(currentNode, "left");
+
+                if (rightNode != null) {
+                    queue.push(rightNode);
+                    setValueForNonPublicField(currentNode, "right", null);
+                }
+
+                if (leftNode != null) {
+                    queue.push(leftNode);
+                    setValueForNonPublicField(currentNode, "left", null);
+                }
+            }
+        }
+    }
+
+
+
+}
--- src/main/java/util/ReflectionUtil.java
+++ src/main/java/util/ReflectionUtil.java
@@ -12,4 +12,23 @@ public final class ReflectionUtil {
         field.setAccessible(true);
         return field.get(source);
     }
+
+    public static final void setValueForNonPublicField(Object target,
+                                                       String nameOfFieldToBeInjected,
+                                                       Object injectedValue)
+            throws NoSuchFieldException, IllegalAccessException {
+        Field field = target.getClass().getDeclaredField(nameOfFieldToBeInjected);
+        field.setAccessible(true);
+        field.set(target, injectedValue);
+    }
+
+    public static final boolean isLeaf(Object treeNode) throws NoSuchFieldException, IllegalAccessException {
+        Object leftNode = getNonPublicFieldAsObject(treeNode, "left");
+        Object rightNode = getNonPublicFieldAsObject(treeNode, "right");
+
+        if (leftNode == null){
+            return rightNode == null;
+        }
+        return false;
+    }
 }
