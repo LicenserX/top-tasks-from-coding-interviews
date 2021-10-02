3b61de272c8e9f5b0de3168fe26e38b91d0c712e
LicenserX <Licensent@gmail.com>
2021-10-02 13:24:48 +0300

    Util was refactored.

--- src/main/java/util/SortingUtil.java
+++ src/main/java/util/SortingUtil.java
@@ -4,8 +4,8 @@ public final class SortingUtil {
     private SortingUtil() {
     }
 
-    public static final void exchange(Integer[] a, int i, int j) {
-        Integer iValue = a[i];
+    public static final void exchange(Object[] a, int i, int j) {
+        Object iValue = a[i];
         a[i] = a[j];
         a[j] = iValue;
     }
