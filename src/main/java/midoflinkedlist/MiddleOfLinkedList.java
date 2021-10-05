8a22f55a2ecc83e8ccd772d7f765cb7a70d0595c
LicenserX <Licensent@gmail.com>
2021-10-05 15:50:27 +0300

    Task was fixed.

--- src/main/java/midoflinkedlist/MiddleOfLinkedList.java
+++ src/main/java/midoflinkedlist/MiddleOfLinkedList.java
@@ -1,16 +1,6 @@
-/*
- * Copyright 2021 Russian Post
- *
- * This source code is Russian Post Confidential Proprietary.
- * This software is protected by copyright. All rights and titles are reserved.
- * You shall not use, copy, distribute, modify, decompile, disassemble or reverse engineer the software.
- * Otherwise this violation would be treated by law and would be subject to legal prosecution.
- * Legal use of the software provides receipt of a license from the right holder only.
- */
 package midoflinkedlist;
 
 import java.util.LinkedList;
-import java.util.ListIterator;
 
 public class MiddleOfLinkedList {
     public static void main(String[] args) {
@@ -31,13 +21,8 @@ public class MiddleOfLinkedList {
     }
 
     public static Integer getMiddleElementOfLinkedList(LinkedList<Integer> linkedList) {
-        ListIterator<Integer> iterator = linkedList.listIterator();
-        int mid = linkedList.size() / 2;
+        int mid = linkedList.size() / 2 - 1;
 
-        while (iterator.hasNext() && mid != 0) {
-            iterator.next();
-            mid--;
-        }
-        return iterator.next();
+        return linkedList.listIterator(mid).next();
     }
 }
